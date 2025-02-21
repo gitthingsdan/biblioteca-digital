6888496e6fcac8bf9d62f5f8a75f752f96cfe1fc
gitthingsdan <dvillarroela@pm.me>
2025-02-21 20:37:42 -0300

    Commit inicial

---
+++ js/clases.js
@@ -0,0 +1,13 @@
+class Libro {
+	constructor(nombreArchivo, titulo, autor, formato, editorial, ano_publicacion, materia_termino_geografico, idioma, genero) {
+		this.nombreArchivo = nombreArchivo;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.formato = formato;
+		this.editorial = editorial;
+		this.ano_publicacion = ano_publicacion;
+		this.materia_termino_geografico = materia_termino_geografico;
+		this.idioma = idioma;
+		this.genero = genero;
+	}
+}
