1455f60bdd9bebbcaaed0b85a572316ad0c4501b
gitthingsdan <dvillarroela@pm.me>
2025-02-22 01:28:01 -0300

    Implementa funcionalidad básica de registro de usuario

--- js/main.js
+++ js/main.js
@@ -1,15 +1,44 @@
-/* FORMULARIO DE REGISTRO */
-/* Al hacer clic en el botón "Registrarse", debería abrirse un modal con un formulario de registro.
-El formulario tiene los siguientes campos:
-- RUT
-- Contraseña (2 veces)
-El formulario tiene un botón "Registrarse". El formulario debería también tener un botón "Cerrar" que permita cerrar el modal.
-Cuando se hace clic en el botón "Registrarse", se debe hacer lo siguiente:
-- Validar que todos los campos estén completos. Si no lo están, mostrar un mensaje de error.
-- Validar que las contraseñas coincidan. Si no coinciden, mostrar un mensaje de error.
-- Si todos los campos están completos y las contraseñas coinciden, mostrar un mensaje de éxito.
-El modal debería cerrarse automáticamente después de 5 segundos.
-Al concluir este proceso, la sesión del usuario debería haberse iniciado automáticamente.
-El formulario no debería enviarse a ningún lado. Los datos deberían almacenarse localmente en el navegador. */
+/* PÁGINA DE INICIO */
+const navMenuBotonSesion = document.getElementById("header_nav_menu_item_linksesion");
 
+/* REGISTRO */
 // Variables
+const patronRut = /^\d{7,8}-[0-9kK]$/;
+const rut = document.getElementById('main_formularioregistro_rut');
+const contrasena1 = document.getElementById('main_formularioregistro_contrasena1');
+const contrasena2 = document.getElementById('main_formularioregistro_contrasena2');
+const botonRegistro = document.getElementById('main_formularioregistro_botonregistro');
+
+// "Event listeners"
+rut?.addEventListener('focusout', () => {
+	if (!patronRut.test(rut.value)) {
+		rut.style.borderColor = 'red';
+	} else {
+		rut.style.borderColor = 'inherit';
+	};
+});
+botonRegistro?.addEventListener('click', registrarse);
+
+// Funciones
+function registrarse() {
+	if (rut.value === '' || contrasena1.value === '' || contrasena2.value === '') {
+		alert('Por favor, complete todos los campos.');
+	} else if (!patronRut.test(rut.value)) {
+		alert('RUT inválido.');
+	} else if (contrasena1.value !== contrasena2.value) {
+		alert('Las contraseñas no coinciden.');
+	} else {
+		const usuario = new Usuario(rut.value, contrasena1.value);
+		localStorage.setItem('usuario', JSON.stringify(usuario));
+		alert('¡Registro exitoso!');
+		setTimeout(() => window.location.href = '../index.html', 3000);
+	}
+}
+
+/* INICIO DE SESIÓN */
+// Variables
+
+// Funciones
+function iniciarSesion() {
+
+}
