07928d5808c8354d552d2240932c25646bda3878
gitthingsdan <dvillarroela@pm.me>
2025-02-21 22:36:29 -0300

    Realiza versión inicial sign up/sign in, y ajusta requerimientos

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 /* FORMULARIO DE REGISTRO */
 /* Al hacer clic en el botón "Registrarse", debería abrirse un modal con un formulario de registro.
 El formulario tiene los siguientes campos:
-- Nombre
-- Apellido
-- Email
+- RUT
 - Contraseña (2 veces)
 El formulario tiene un botón "Registrarse". El formulario debería también tener un botón "Cerrar" que permita cerrar el modal.
 Cuando se hace clic en el botón "Registrarse", se debe hacer lo siguiente:
 - Validar que todos los campos estén completos. Si no lo están, mostrar un mensaje de error.
-- Validar que el campo de email tenga un formato de email válido. Si no lo tiene, mostrar un mensaje de error.
 - Validar que las contraseñas coincidan. Si no coinciden, mostrar un mensaje de error.
 - Si todos los campos están completos y las contraseñas coinciden, mostrar un mensaje de éxito.
 El modal debería cerrarse automáticamente después de 5 segundos.
