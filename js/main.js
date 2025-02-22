839a12024ad6943616d1c2f5a2c8c0b67465dc5e
gitthingsdan <dvillarroela@pm.me>
2025-02-21 21:48:03 -0300

    Define requisitos básicos para sign up/sign in

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/* FORMULARIO DE REGISTRO */
+/* Al hacer clic en el botón "Registrarse", debería abrirse un modal con un formulario de registro.
+El formulario tiene los siguientes campos:
+- Nombre
+- Apellido
+- Email
+- Contraseña (2 veces)
+El formulario tiene un botón "Registrarse". El formulario debería también tener un botón "Cerrar" que permita cerrar el modal.
+Cuando se hace clic en el botón "Registrarse", se debe hacer lo siguiente:
+- Validar que todos los campos estén completos. Si no lo están, mostrar un mensaje de error.
+- Validar que el campo de email tenga un formato de email válido. Si no lo tiene, mostrar un mensaje de error.
+- Validar que las contraseñas coincidan. Si no coinciden, mostrar un mensaje de error.
+- Si todos los campos están completos y las contraseñas coinciden, mostrar un mensaje de éxito.
+El modal debería cerrarse automáticamente después de 5 segundos.
+Al concluir este proceso, la sesión del usuario debería haberse iniciado automáticamente.
+El formulario no debería enviarse a ningún lado. Los datos deberían almacenarse localmente en el navegador. */
+
+// Variables
