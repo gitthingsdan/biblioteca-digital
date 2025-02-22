de0ae6a917ffea785b17e5525cba1fa2089c1ab2
gitthingsdan <dvillarroela@pm.me>
2025-02-21 22:34:49 -0300

    Simplifica estructura Libro y crea clase Usuario

--- js/clases.js
+++ js/clases.js
@@ -1,13 +1,17 @@
 class Libro {
-	constructor(nombreArchivo, titulo, autor, formato, editorial, ano_publicacion, materia_termino_geografico, idioma, genero) {
+	constructor(nombreArchivo, titulo, autor, editorial, ano_publicacion, tematica) {
 		this.nombreArchivo = nombreArchivo;
 		this.titulo = titulo;
 		this.autor = autor;
-		this.formato = formato;
 		this.editorial = editorial;
 		this.ano_publicacion = ano_publicacion;
-		this.materia_termino_geografico = materia_termino_geografico;
-		this.idioma = idioma;
-		this.genero = genero;
+		this.tematica = tematica;
+	}
+}
+
+class Usuario {
+	constructor(rut, contrasena) {
+		this.rut = rut;
+		this.contrasena = contrasena;
 	}
 }
