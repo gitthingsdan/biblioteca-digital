78d0ec8b3aa5250473ac393171f390c6603087e7
gitthingsdan <dvillarroela@pm.me>
2025-03-02 21:55:57 -0300

    Añade reseña de libros

--- js/clases.js
+++ js/clases.js
@@ -1,11 +1,12 @@
 class Libro {
-	constructor(nombreArchivo, titulo, autor, editorial, ano_publicacion, tematica) {
+	constructor(nombreArchivo, titulo, autor, editorial, ano_publicacion, tematica, resena) {
 		this.nombreArchivo = nombreArchivo;
 		this.titulo = titulo;
 		this.autor = autor;
 		this.editorial = editorial;
 		this.ano_publicacion = ano_publicacion;
 		this.tematica = tematica;
+		this.resena = resena;
 	}
 }
 
